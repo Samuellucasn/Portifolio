42018cef8e96729bf79c0f71d08401414d66ce28
Samuellucasn <samuellucasnogueira@gmail.com>
2023-01-02 21:45:53 -0300

    delete roundShape component

--- src/components/RoundShape/index.tsx
+++
@@ -1,9 +0,0 @@
-import './style.scss'
-
-const RoundShape: React.FC<any> = ({className}) => {
-    return(
-        <div className={`round-shape ${className}`}/>
-    )
-}
-
-export default RoundShape
--- src/components/pages/HomeSection/index.tsx
+++ src/components/pages/HomeSection/index.tsx
@@ -1,18 +1,17 @@
-import RoundShape from '../../RoundShape'
 import './style.scss'
 
 const Home: React.FC<any> = () => {
     return (
         <main className='home-main'>
             <div className="saturn-wrapper">
-                <RoundShape className='saturn' />
+                <div className='saturn' />
             </div>
             <div className="jupiter-wrapper">
-                <RoundShape className='jupiter' />
+                <div className='jupiter' />
             </div>
             <div className="earth-wrapper">
-                <RoundShape className='earth' />
-                <RoundShape className='moon' />
+                <div className='earth' />
+                <div className='moon' />
             </div>
         </main>
     )
