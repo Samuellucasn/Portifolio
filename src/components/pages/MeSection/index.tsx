350b64911cba86353989621709940a9c0040ac43
Samuellucasn <samuellucasnogueira@gmail.com>
2023-01-05 20:52:32 -0300

    feat layout changes

--- src/components/pages/MeSection/index.tsx
+++ src/components/pages/MeSection/index.tsx
@@ -10,8 +10,7 @@ const Me: React.FC<any> = () => {
             <div className="about-me">
                 <div className="perfil-img" />
                 <h1>Samuel Lucas</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat iusto reiciendis, eaque natus, unde recusandae odio animi, sequi saepe quam consectetur ullam perferendis pariatur. Adipisci provident dolores magnam? Alias, eos?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe qui, quibusdam fugit ex quia corporis modi quas quis ullam, vitae quidem hic! Adipisci dolorem commodi aperiam et unde distinctio voluptate?
-                Numquam nostrum illum ipsa laboriosam recusandae porro quisquam! Placeat reiciendis laborum repellendus accusamus vitae ratione incidunt odio omnis maxime, velit amet nam fugiat facere vero ex inventore perferendis ipsum error?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat iusto reiciendis, eaque natus, unde recusandae odio animi, sequi saepe quam consectetur ullam perferendis pariatur. Adipisci provident dolores magnam? Alias, eos?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe qui, quibusdam fugit ex quia corporis modi quas quis ullam, vitae quidem hic!</p>
             </div>
 
             <div className="wrapper">
