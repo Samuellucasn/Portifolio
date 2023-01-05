dbf6df5ea9e9c79634768333c877f6202b8fddc5
Samuellucasn <samuellucasnogueira@gmail.com>
2023-01-05 17:15:11 -0300

    feat about background

--- src/components/pages/MeSection/index.tsx
+++ src/components/pages/MeSection/index.tsx
@@ -3,7 +3,25 @@ import './style.scss'
 const Me: React.FC<any> = () => {
     return (
         <main className='about-main'>
-            
+            <img src="/src/assets/red-moon.png" alt='red-moon' />
+
+            <div className="about-me">
+                <div className="perfil-img" />
+                
+            </div>
+
+            <div className="contacts">
+                <ul>
+                    <li>GitHub</li>
+                    <li>Linkedin</li>
+                    <li>E-mail</li>
+                </ul>
+            </div>
+
+            <div className="stacks">
+
+            </div>
+
         </main>
     )
 }
