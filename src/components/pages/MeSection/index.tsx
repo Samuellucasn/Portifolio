3a9b61a324a1a2f2bf431f35ae4886c0c7983065
Samuellucasn <samuellucasnogueira@gmail.com>
2023-01-05 19:35:50 -0300

    feat about section layout

--- src/components/pages/MeSection/index.tsx
+++ src/components/pages/MeSection/index.tsx
@@ -1,5 +1,7 @@
 import './style.scss'
 
+import Button from '../../Button'
+
 const Me: React.FC<any> = () => {
     return (
         <main className='about-main'>
@@ -7,19 +9,28 @@ const Me: React.FC<any> = () => {
 
             <div className="about-me">
                 <div className="perfil-img" />
-                
+                <h1>Samuel Lucas</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat iusto reiciendis, eaque natus, unde recusandae odio animi, sequi saepe quam consectetur ullam perferendis pariatur. Adipisci provident dolores magnam? Alias, eos?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe qui, quibusdam fugit ex quia corporis modi quas quis ullam, vitae quidem hic! Adipisci dolorem commodi aperiam et unde distinctio voluptate?
+                Numquam nostrum illum ipsa laboriosam recusandae porro quisquam! Placeat reiciendis laborum repellendus accusamus vitae ratione incidunt odio omnis maxime, velit amet nam fugiat facere vero ex inventore perferendis ipsum error?</p>
             </div>
 
-            <div className="contacts">
-                <ul>
-                    <li>GitHub</li>
-                    <li>Linkedin</li>
-                    <li>E-mail</li>
-                </ul>
-            </div>
+            <div className="wrapper">
+                <div className="contacts">
+                    <ul>
+                        <li>GitHub</li>
+                        <li>Linkedin</li>
+                        <li>E-mail</li>
+                    </ul>
+                </div>
 
-            <div className="stacks">
-
+                <div className="stacks">
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                </div>
             </div>
 
         </main>
