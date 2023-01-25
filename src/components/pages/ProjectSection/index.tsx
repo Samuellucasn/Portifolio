0a043eabe33c6201e19773e201d4e8359f2a6107
Samuellucasn <samuellucasnogueira@gmail.com>
2023-01-24 21:07:59 -0300

    change project style

--- src/components/pages/ProjectSection/index.tsx
+++ src/components/pages/ProjectSection/index.tsx
@@ -11,31 +11,31 @@ const Projects: React.FC<any> = () => {
                 <div className="thumbnail" />
                 <div className="links">
                     <a href=""><button className="code" > <FiGithub /> code </button></a>
-                    <button className="website" > <MdComputer /> website </button>
+                    <a href=""><button className="website" > <MdComputer /> website </button></a>
                 </div>
             </div>
+
             <div className='project-two'>
                 <div className="thumbnail" />
-                <div className="info">
-                    <p></p>
-                    <div className="code" />
-                    <div className="website" />
+                <div className="links">
+                    <a href=""><button className="code" > <FiGithub /> code </button></a>
+                    <a href=""><button className="website" > <MdComputer /> website </button></a>
                 </div>
             </div>
+
             <div className='project-three'>
                 <div className="thumbnail" />
-                <div className="info">
-                    <p></p>
-                    <div className="code" />
-                    <div className="website" />
+                <div className="links">
+                    <a href=""><button className="code" > <FiGithub /> code </button></a>
+                    <a href=""><button className="website" > <MdComputer /> website </button></a>
                 </div>
             </div>
+
             <div className='project-four'>
                 <div className="thumbnail" />
-                <div className="info">
-                    <p></p>
-                    <div className="code" />
-                    <div className="website" />
+                <div className="links">
+                    <a href=""><button className="code" > <FiGithub /> code </button></a>
+                    <a href=""><button className="website" > <MdComputer /> website </button></a>
                 </div>
             </div>
         </section>
