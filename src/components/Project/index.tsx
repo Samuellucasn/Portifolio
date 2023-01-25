4de14d6cb81cc9d17f39c9a47a2b8dc2b3abdeba
Samuellucasn <samuellucasnogueira@gmail.com>
2023-01-25 12:56:20 -0300

    feat Project component style

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -3,10 +3,10 @@ import './style.scss'
 import {FiGithub} from 'react-icons/fi'
 import {MdComputer} from 'react-icons/md'
 
-const Project: React.FC<any> = () => {
+const Project: React.FC<any> = ({thumbnail}) => {
     return (
         <div className='project'>
-                <div className="thumbnail" />
+                <div className={thumbnail} />
                 <div className="links">
                     <a href=""><button className="code" > <FiGithub /> code </button></a>
                     <a href=""><button className="website" > <MdComputer /> website </button></a>
--- src/components/pages/ProjectSection/index.tsx
+++ src/components/pages/ProjectSection/index.tsx
@@ -2,17 +2,13 @@ import './style.scss'
 
 import Project from '../../Project'
 
-import {FiGithub} from 'react-icons/fi'
-import {MdComputer} from 'react-icons/md'
-
-
 const Projects: React.FC<any> = () => {
     return (
         <section className='projects'>
-            <Project className='project-one' />
-            <Project className='project-two' />
-            <Project className='project-three' />
-            <Project className='project-four' />
+            <Project thumbnail='thumbnail-one' />
+            <Project thumbnail='thumbnail-two' />
+            <Project thumbnail='thumbnail-three' />
+            <Project thumbnail='thumbnail-four' />
         </section>
     )
 }
